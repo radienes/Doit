5c39b49bcb464678bd7ed541c403327ce3219996
Napatsorn Lam Sakulsuwan <PATCHUNG@Napatsorns-MacBook-Air.local>
2016-12-23 00:08:05 +0700

    Class Task added

---
+++  Task.swift
@@ -0,0 +1,16 @@
+//
+//   Task.swift
+//  Doit
+//
+//  Created by Napatsorn Lam Sakulsuwan on 12/22/16.
+//  Copyright © 2016 Napatsorn Lam Sakulsuwan. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+    
+}
